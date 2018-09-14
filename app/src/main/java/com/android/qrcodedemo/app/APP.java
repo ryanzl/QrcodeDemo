9a345739c37ac06804ba92da39acf38a81dc3b73
chenxuebin <799536767@qq.com>
2018-09-14 11:42:29 +0800

    #导入所需文件

---
+++ app/src/main/java/com/android/qrcodedemo/app/APP.java
@@ -0,0 +1,23 @@
+package com.android.qrcodedemo.app;
+
+import android.app.Application;
+
+/**
+ * application
+ */
+
+public class APP extends Application {
+
+    private static APP mApp;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApp = this;
+    }
+
+    public static APP get(){
+        return mApp;
+    }
+
+}
---
+++ app/src/main/java/com/android/qrcodedemo/ui/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android.qrcodedemo.ui;
+
+import com.android.qrcodedemo.R;
+import com.android.qrcodedemo.app.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+}
